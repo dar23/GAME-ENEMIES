628c7f558b2e87b1fec382ee65fd7a5a3c752152
dar23 <dariuszhozer23@gmail.com>
2023-03-11 15:37:23 +0100

    game

--- rock.js
+++ rock.js
@@ -1,25 +1,38 @@
 
 function rock(){
 
-    
+  
   
     c.fillStyle = "red";
-    c.fillRect(initX, initY, 70,30);
-    
-    
+   
+   c.fillRect(initX, initY, 70,30);
+   
+
     
     
     initY++;  // współrzędna Y zwiększa się o 1 pixel (przesuwa się w dół wg osi y);
     
+  
     
-    
-    if(initY>=400){
+    if(initY>=400 && initY>=10){
     
          initY=0;
          initX=Math.random()*630;
          
     }
     
+   
+
+
     }
+  
+
+
+   
+
+
+
+
     
+
     
